b4484b85401f926fac7318e030685a8262e5da62
Andrey Salnikov <andrycalnicow@gmail.com>
2025-07-23 18:31:55 +0400

    Add csv adapter

---
+++ adapter/csv/client.go
@@ -0,0 +1,29 @@
+package csv
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+type Client struct{}
+
+func NewClient() *Client {
+	return &Client{}
+}
+
+func (cc *Client) ReadCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return records
+}
