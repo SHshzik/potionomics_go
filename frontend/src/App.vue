3bfb07318f48d34c7266a3e97a11a1774b15c0ca
Andrey Salnikov <andrycalnicow@gmail.com>
2025-07-24 20:35:52 +0400

    update vite

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png"/>
   <HelloWorld/>
 </template>
 
